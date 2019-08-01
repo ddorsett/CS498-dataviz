844679ee361f19cbb3c971d519eda90a904eda52
ddorsett <david.dorsett@anythingwithsoftware.com>
2019-07-31 23:54:57 -0400

    clean up sizing

--- drawWeatherTrend.js
+++ drawWeatherTrend.js
@@ -1,3 +1,7 @@
+
+//  create D3 temperature and precip graphs with various options
+//  D. Dorsett 20-Jul-2019
+
 const stationList = {
 	["USW00014764"]: "Portland, ME",
 	["USW00014739"]: "Boston, MA",
@@ -11,6 +15,7 @@ const stationList = {
 	["USW00092811"]: "Miami Beach, FL"
 }
 
+// simple least-sqaure calculation based on x and y array argument, returning a function to be used for interpolation
 function linearFit(xSeries, ySeries) {
 	let sum = function (p, c) {
 		return p + c
@@ -33,15 +38,19 @@ function linearFit(xSeries, ySeries) {
 	})
 }
 
+// create SVG graphs of temperature anomalies for a season/station array
 async function drawSeasonTemp(parentSelector, replace, stations, season, startYear, highTemp, showActuals, animate, showEvents, showAnnotations, showTrend) {
+	// 4 data sets
 	const baseline = await d3.csv('data/season_baseline.csv')
 	let data = await d3.csv('data/seasons.csv')
 	let events = await d3.csv('data/weatherevents.csv')
 	let anndata = await d3.csv('data/events.csv')
 
+	// filter by station and season
 	data = data.filter(d => Number(d.YEAR) >= startYear && d.SEASON === season)
 	events = events.filter(d => {return (Number(d.YEAR) >= startYear && d.SEASON === season && d.TYPE === (highTemp ? "TMAX" : "TMIN"))})
-	// create annotation data structure
+
+	// create annotation data structure needed for d3-annotation
 	let annotations = []
 	for (let a = 0; a < anndata.length; a++) {
 		annotations.push({
@@ -55,24 +64,32 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 	}
 
 	const margin = { top: 50, right: 20, bottom: 20, left: 20 },
-		width = 1000 - margin.left - margin.right,
-		height = 200
+		width = 800 - margin.left - margin.right,
+		height = 125
+
+	// scales
 	let x = d3.scaleTime().range([0, width]).domain([new Date(startYear, 0), new Date(2018, 11)])
 	let y = d3.scaleLinear().range([height, 0]).domain([-10, 10])
+	// this is an alternate y scale used to place annotations on rows below the graph of the values
 	let yann = d3.scaleLinear().range([height+150, height+20]).domain([1,5])
+
+	// axes
 	let xAxis = d3.axisBottom().scale(x)
 	let yAxis = d3.axisLeft().scale(y).ticks(5)
 	let tooltip = d3.select("#tooltip")
 
+	// line generator (skip any undefined data)
 	let line = d3.line()
 		.x(d => { return x(d.date) })
 		.y(d => { return y(d.temp) })
 		.defined(d => { return d != -999 })
 
 	let svg
-
+	// append a grahph for each station in the array passed
 	for (let s = 0; s < stations.length; s++) {
 		let station = stations[s]
+
+		// transform seasonabl average into anomaly values
 		let sdata = data.filter(d => d.STATION === station)
 		let tempBaseline = 0
 		for (let i = 0; i < baseline.length; i++) {
@@ -92,7 +109,7 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 		}
 		let edata = events.filter(d => d.STATION === station)
 
-		// is there already a chart in the parent?? If so, replace it
+		// is there already a chart in the parent?? If so, replace it (if asked)
 		let existing = !d3.select(parentSelector + " div").empty()
 		if (replace && existing) {
 			d3.select(parentSelector + " div").remove()
@@ -101,10 +118,9 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 			.append('div').attr('id', station).attr('class', 'station')
 			.append('svg')
 			.attr('viewBox', [0, 0, (width + margin.left + margin.right), height + margin.top + margin.bottom + (showAnnotations ? 225 : 0)].join(' '))
-			// .attr('width', width + margin.left + margin.right)
-			// .attr('height', height + margin.top + margin.bottom + (showAnnotations ? 225 : 0))
 		let bounds = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`)
 
+		// set up tooltips if we're displaying actuals data
 		if (showActuals) {
 			let tooltipCircle = bounds.append("circle").attr("r", 4).attr("stroke", "#af9358").attr("fill", "white").attr("stroke-width", 2).style("opacity", 0)
 			let overlay = bounds.append('rect')
@@ -133,7 +149,7 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 				})
 		}
 
-		// axes and zero-line
+		// draw axes and zero-line
 		let x1 = new Date(startYear, 0)
 		let x2 = new Date(2018, 0)
 		let yearWidth = x(x2) - x(new Date(2017, 0))
@@ -160,6 +176,7 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 			.attr("y2", d => { return y(d[3]) })
 			.attr("stroke-dasharray", "5,5")
 
+		// temperature events (e.g. heat waves) are drawn on the zero line
 		if (showEvents) {
 			bounds.selectAll(".event").data(edata).enter().append("circle")
 				.attr("cx", d => { return x(new Date(d.YEAR, 0)) })
@@ -195,6 +212,7 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 				.attr("stroke-dasharray", "10,10")
 		}
 
+		// finally draw the temp anomaly line
 		if (showActuals) {
 			let path = bounds.append("path").datum(anomalyData).attr("class", "templine " + season).attr("id", "p"+station).attr("d", line)
 			if (animate) {
@@ -209,6 +227,8 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 			}
 		}
 
+		// and finish up with the annotations, which are assigned a "row" to be displayed on below the value-graph area
+		// done with an alternate y scale
 		if (showAnnotations) {
 			const makeAnnotations = d3.annotation()
 				.textWrap(85)
@@ -222,9 +242,9 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 			let trendline = bounds.selectAll(".eventline").data(annotations).enter()
 				.append("line")
 				.attr("class", "annotationline")
-				.attr("x1", d => { return x(new Date(Number(d.data.YEAR), Number(d.data.MONTH))) })
-				.attr("y1", d => { return yann(Number(d.data.ROW)) })
-				.attr("x2", d => { return x(new Date(Number(d.data.YEAR), Number(d.data.MONTH))) })
+				.attr("x1", d => { return x(new Date(d.data.YEAR, d.data.MONTH)) })
+				.attr("y1", d => { return yann(d.data.ROW) })
+				.attr("x2", d => { return x(new Date(d.data.YEAR, d.data.MONTH)) })
 				.attr("y2", d => { return y(0) })
 				.attr("stroke-dasharray", "5,5")
 		}
@@ -232,24 +252,33 @@ async function drawSeasonTemp(parentSelector, replace, stations, season, startYe
 	return svg
 }
 
+// create SVG graphs of precip totals for a season/station array
 async function drawSeasonPrecip(parentSelector, replace, stations, season, startYear, showActuals, animate, showEvents) {
+	// data sets we need
 	const baseline = await d3.csv('data/season_baseline.csv')
 	let data = await d3.csv('data/seasons.csv')
 	let events = await d3.csv('data/weatherevents.csv')
 
+	// filter them down
 	data = data.filter(d => Number(d.YEAR) >= startYear && d.SEASON === season)
 	events = events.filter(d => Number(d.YEAR) >= startYear && d.SEASON === season && d.TYPE === "PRCP")
+
+	// want the Y axis to be responive to the data since the range could be large, but if we're generating a
+	//  set of graphs all the Y axes should be the same
 	let maxY = Math.ceil(Math.max(...data.map(d => { return parseFloat(d.PRCP_TOTAL) }))/5) * 5
 
 	const margin = { top: 50, right: 20, bottom: 20, left: 20 },
-		width = 1000 - margin.left - margin.right,
-		height = 200
+		width = 800 - margin.left - margin.right,
+		height = 125
+
+	// scales and axes
 	let x = d3.scaleBand().range([0, width]).domain(d3.range(startYear, 2019))
 	let y = d3.scaleLinear().range([height, 0]).domain([0, Math.round(maxY)])
 	let xAxis = d3.axisBottom().scale(x).tickValues(x.domain().filter((d,i) => { return !(d%5)}))
 	let yAxis = d3.axisLeft().scale(y).ticks(5)
 	let tooltip = d3.select("#tooltip")
 
+	// each station gets a graph
 	for (let s = 0; s < stations.length; s++) {
 		let station = stations[s]
 
@@ -261,14 +290,14 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 		let svg = d3.select(parentSelector).append('div').attr('id', station).attr('class', 'station')
 			.append("svg")
 			.attr('viewBox', [0, 0, (width + margin.left + margin.right), height + margin.top + margin.bottom].join(' '))
-			// .attr("width", width + margin.left + margin.right)
-			// .attr("height", height + margin.top + margin.bottom)
 			.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
+		// station and event data for the graph
 		let sdata = data.filter(d => d.STATION === station)
 		let edata = events.filter(d => d.STATION === station)
 
+		// baseline value showing average precip for the season/station
 		let prcpBaseline = 0
 		for (let i = 0; i < baseline.length; i++) {
 			if (baseline[i].STATION === station && baseline[i].SEASON === season) {
@@ -276,6 +305,8 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 				break
 			}
 		}
+
+		// reducing the data array for the bar graphing (not strictly necessary, tho)
 		let prcpData = []
 		for (let i = 0; i < sdata.length; i++) {
 			prcpData.push({
@@ -285,7 +316,7 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 			})
 		}
 
-		// axes and zero-line
+		// draw axes and zero-line and title
 		svg.append("g")
 			.attr("class", "x axis")
 			.attr("transform", "translate(0," + height + ")")
@@ -301,6 +332,7 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 			.style("font-family", "Arial, Helvetica, sans-serif")
 			.text(`${stationList[station]} Total ${season[0].toUpperCase()}${season.substring(1)} Precipitation from ${startYear} to 2019`);
 
+		// actual total precip values
 		if (showActuals) {
 			let bars = svg.selectAll(".bar").data(prcpData).enter().append("rect")
 				.attr("class", (d => {
@@ -317,6 +349,7 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 				})
 				.on("mouseout", function() { tooltip.style("opacity", 0) })
 
+			// either finish up the y/height attributes or make them invisible and transition them in
 			if (animate) {
 				bars.attr("y", function(d) { return height })
 					.attr("height", 0)
@@ -330,6 +363,8 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 					.attr("height", d => { return height - y(d.rain) })
 			}
 		}
+
+		// the "zeroline" here is the seasonal average baseline
 		svg.selectAll(".zeroline").data([[startYear, prcpBaseline, 2018, prcpBaseline]]).enter().append("line")
 			.attr("class", "zeroline")
 			.attr("x1", d => { return x(d[0]) })
@@ -339,6 +374,7 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 			.attr("stroke-dasharray", "5,5")
 		let lastYear, lastSide = 1, kick = 0
 
+		// precip events on the baseline as color-coded circles
 		if (showEvents) {
 			let stormCirle = svg.selectAll(".event").data(edata).enter().append("circle")
 				.attr("cx", d => { return x(d.YEAR)+x.bandwidth()/2 })
@@ -352,6 +388,7 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 				tooltip.select("#tooltip-message").text(`${d.EVENT} ${d.LABEL}`)
 				})
 				.on("mouseout", function() { tooltip.style("opacity", 0) })
+			// if we're animating, make the events swell back and forth
 			if (animate) {
 				repeatSwell()
 			}
@@ -367,6 +404,7 @@ async function drawSeasonPrecip(parentSelector, replace, stations, season, start
 					.on("end", repeatSwell)
 			}
 
+			// add text of the event ot the graph, moving it around if it's close together in years to avoid overlap if possible!
 			svg.selectAll(".eventtext").data(edata).enter()
 				.append("text")
 				.attr("x", d => { return x(d.YEAR)+x.bandwidth()/2 })
